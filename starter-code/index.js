2b44e58904cd02c0d97f161dcd800415a4e80f51
Brandon <bkthree56@gmail.com>
2021-12-17 17:49:44 -0600

    Finished home page.

---
+++ starter-code/index.js
@@ -0,0 +1,17 @@
+// When someone clicks the hamburger btn
+// if the nav is closed, open it
+// if the nav is open, close it
+
+const nav = document.querySelector(".primary-navigation");
+const navToggle = document.querySelector(".mobile-nav-toggle");
+
+navToggle.addEventListener("click", () => {
+    const visibility = nav.getAttribute("data-visible");
+    if (visibility === "false") {
+        nav.setAttribute("data-visible", true);
+        navToggle.setAttribute("aria-expanded", true);
+    } else {
+        nav.setAttribute("data-visible", false);
+        navToggle.setAttribute("aria-expanded", false);
+    }
+})
